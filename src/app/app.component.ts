48a6b490d5992eb44160897d6f340409f17b055f
Kshitij <kshitij.kanojiya@abjayon.com>
2025-03-28 11:10:12 +0530

    add structure changes in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from '../components/navbar/navbar.component';
+import { FormComponent } from '../components/form/form.component';
+import { BoardComponent } from '../components/board/board.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NavbarComponent, RouterOutlet, FormComponent, BoardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular';
